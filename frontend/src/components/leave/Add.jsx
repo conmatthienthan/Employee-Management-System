a23fa86adbf4a1c981f417f61e1b1eafdafdec5a
conmatthienthan <tranhieunhat280@gmail.com>
2025-11-02 07:15:20 +0700

    init

--- frontend/src/components/leave/Add.jsx
+++ frontend/src/components/leave/Add.jsx
@@ -42,7 +42,6 @@ const Add = () => {
       alert(error.response?.data?.error || "Không thể gửi đơn nghỉ phép");
     }
   };
-      console.log("🧑‍💼 User hiện tại:", user);
   return (
     <div className="max-w-4xl mx-auto mt-10 bg-white p-8 rounded-md shadow-md">
       <h2 className="text-2xl font-bold mb-6 text-teal-700 text-center">
